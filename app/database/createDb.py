613bafbb1b3bd5b32fd06c70644b0de29c8adfb7
matheushpr9 <matheushpr9@gmail.com>
2023-10-25 19:54:30 -0300

    Alter secret keys.

--- app/database/createDb.py
+++ app/database/createDb.py
@@ -1,6 +1,10 @@
+from dotenv import load_dotenv
+import os
 from database.Connector import Connector
 
-connector = Connector("matheushpr9", "bZw23JHQYa9OrXLm")
+load_dotenv()
+
+connector = Connector(os.getenv('DATABASE_USER'),  os.getenv('DATABASE_PASSWORD'))
 #a = connector.word_exists(connector.set_word("dependendo"), "portuguese")
 
 b =connector.get_item_by_id("portuguese", 1)
